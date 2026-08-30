0cb8fd170e427fbda93fa929574c1ededf0d898c
unknown <prigge.nicolas@gmail.com>
2026-08-30 09:24:45 -0400

    le mode de bibliotecaire/master

--- livrebiblio.py
+++ livrebiblio.py
@@ -102,3 +102,49 @@ class LivreBiblio:
     def save_file(self):
         with open("db/livre.json", "w") as file:
             json.dump(self.data, file, indent=4)
+
+    def add_livre(self):
+        titre = input("entrez le titre du livre >>> ")
+        auteur = input("entrez l'auteur >>> ")
+        qte = input("entrez la quantite disponible >>>")
+        dispo = input("est il disponible en ce moment [Y/N] >>> ")
+        id = input("entrez l'id du livre >>> ")
+
+        if dispo == "Y" or dispo == "y" or dispo == "yes":
+            dispo = True
+
+        else:
+            dispo = False
+
+        new_book = {
+            "id": id,
+            "titre": titre,
+            "auteur": auteur,
+            "quantite_totale": qte,
+            "disponible": dispo
+        }
+
+        self.data.append(new_book)
+        self.save_file()
+
+    def retirer_livre(self):
+        book = self.get_input().lower().strip()
+        resultat = [
+            livre for livre in self.data
+            if book in livre["titre"].lower()
+        ]
+
+        if resultat:
+            for livre in resultat:
+                info = {
+                    "id": livre["id"],
+                    "titre": livre["titre"],
+                    "auteur": livre["auteur"],
+                    "quantite_totale": livre["quantite_totale"],
+                    "disponible": livre["disponible"]
+                }
+            if info in self.data:
+                print(info)
+                self.data.remove(info)
+            
+        self.save_file()
--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from user import User
 user = User()
 biblio = LivreBiblio()
 
-
-
-
-
 def emprunt():
     livre = biblio.emprunter()
     if livre == -1:
@@ -28,8 +24,11 @@ def rendre():
 
     user.emprunt.remove(titre)
 
-def user_input():
-    return input("que voulez vous faire\n1: emprunter\n2: chercher\n3: retourner un livre\n4: changer le mot de passe\n5: voir cest emprunt actif \n6: quitter \n>>> ")
+def user_input(mode):
+    if mode == 1:
+        return input("que voulez vous faire\n1: emprunter\n2: chercher\n3: retourner un livre\n4: changer le mot de passe\n5: voir cest emprunt actif \n6: quitter \n>>> ")
+    if mode == 2:
+        return input("que voulez vous faire\n1: ajouter un livre\n2: retirer un livre\n3: surpimer un utilisateur\n4: quitter \n>>> ")
 
 def print_emprunt():
     for i in user.emprunt:
@@ -39,24 +38,41 @@ def main():
     if not user.login():
         raise PermissionError("mot de passe ou nom dutilisateur invalide")
 
-    while True:
-        choix = user_input()
-        match choix:
-            case "1":
-                emprunt()
-            case "2":
-                biblio.search()
-            case "3":
-                rendre()
-            case "4":
-                user.change_mdp()
-            case "5":
-                print_emprunt()
-            case "6":
-                break
-            case _:
-                print("commande non reconnu")
-
+    if user.user_name == "sudo":
+        while True:
+            choix = user_input(2)
+            match choix:
+                case "1":
+                    biblio.add_livre()
+                case "2":
+                    biblio.retirer_livre()
+                case "3":
+                    user.delete_user()
+                case "4":
+                    break
+                case _:
+                    print("commande non reconnu")
+            print("\n")
+    else:
+        while True:
+            choix = user_input(1)
+            match choix:
+                case "1":
+                    emprunt()
+                case "2":
+                    biblio.search()
+                case "3":
+                    rendre()
+                case "4":
+                    user.change_mdp()
+                case "5":
+                    print_emprunt()
+                case "6":
+                    break
+                case _:
+                    print("commande non reconnu")
+            print("\n")
+    
     user.logout()
 
 
